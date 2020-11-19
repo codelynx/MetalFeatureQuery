4f974c4d726dce43573effb849bd3662647aebfc
Kaz Yoshikawa <kaz.yoshikawa@gmail.com>
2020-11-19 05:12:47 -0500

    Initial Commit

---
+++ Shared/Metal_Feature_QueryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_Feature_QueryApp.swift
+//  Shared
+//
+//  Created by Kaz Yoshikawa on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct Metal_Feature_QueryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
